5fa365305dc06e6b008c48af518f31b3f50766cd
Alex Bieliakov <bieliakov.alex@gmail.com>
2017-05-18 11:01:47 +0300

    Добавлен пример выравнивания текста по правому краю

--- dialog.cpp
+++ dialog.cpp
@@ -10,6 +10,12 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    QTextCursor cursor=ui->textEdit->textCursor();
+    QTextBlockFormat textBlockFormat=cursor.blockFormat();
+    textBlockFormat.setAlignment(Qt::AlignRight);
+    cursor.mergeBlockFormat(textBlockFormat);
+    ui->textEdit->setTextCursor(cursor);
 }
 
 Dialog::~Dialog()
